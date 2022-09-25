ccfe6551daa23926e4ea2f24dd7ac8963ae3f3a1
Mskauen <mathias.skauen.harestad@gmail.com>
2022-09-25 22:10:22 +0200

    Next setup with Tailwind and Typescript

---
+++ pages/index.tsx
@@ -0,0 +1,32 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <meta charSet="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1 viewport-fit=cover"/>
+        <title>Mathias Skauen Harestad</title>
+      </Head>
+
+    {/* Header */}
+
+    {/* Hero */}
+
+    {/* About */}
+
+    {/* Experience */}
+
+    {/* Skills */}
+
+    {/* Projects */}
+
+    {/* Contact Me */}
+
+    </div>
+  )
+}
+
+export default Home
